2d0e8def9931c5a1de5b8a001498e0c25528e28b
Syed Raza <syed.raza011@gmail.com>
2023-08-23 22:22:33 -0400

    finally app is loading ContentView in ViewControler

--- CatsDataAPP/ViewController.swift
+++ CatsDataAPP/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        loadCatScene()
     }
         //inside root call contentView
     func loadCatScene(){
